af254eb44f33d508601ffdd44e108898fa125ef4
Rafael Santana <rafaelsantana7213@gmail.com>
2026-04-13 08:56:54 -0300

    feat: login screen

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import "react-native-reanimated";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(public)" />
+      <Stack.Screen name="(private)" />
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
